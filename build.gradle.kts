ad81324006c277ecb683488b6fdc8e7df0f0a800
noavarice <noavarice@gmail.com>
2024-04-14 12:53:21 +0300

    Fixed build script indentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    id("java")
+  java
 }
 
 group = "com.github.jsonrecords"
 version = "0.1.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  testImplementation(platform("org.junit:junit-bom:5.10.0"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
